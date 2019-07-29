510686eb40b545109dad3aa5d27b68d5c324dd2f
oussemabelaid <47796584+oussemabelaid@users.noreply.github.com>
2019-07-29 11:36:03 +0100

    Add files via upload
    JavaScriptCheckpoint

---
+++ js/main.js
@@ -0,0 +1,71 @@
+function getNummber(num) {
+        var input_var = document.getElementById('calc-output');
+        switch (num) {
+            case 1:
+                input_var.value += '1';
+                break;
+            case 2:
+                input_var.value += '2';
+                break;
+            case 3:
+                input_var.value += '3';
+                break;
+            case 4:
+                input_var.value += '4';
+                break;
+            case 5:
+                input_var.value += '5';
+                break;
+            case 6:
+                input_var.value += '6';
+                break;
+            case 7:
+                input_var.value += '7';
+                break;
+            case 8:
+                input_var.value += '8';
+                break;
+            case 9:
+                input_var.value += '9';
+                break;
+            case 0:
+                input_var.value += '0';
+                break;
+        }
+    }
+    
+    function getOperand(operand) {
+        var input_var = document.getElementById('calc-output')
+        switch (operand) {
+            case '+':
+                input_var.value += '+';
+                break;
+            case '-':
+                input_var.value += '-';
+                break;
+            case '*':
+                input_var.value += '*';
+                break;
+            case '/':
+                input_var.value += '/';
+                break;
+            case '=':
+                input_var.value += '=';
+                break;
+        }
+    }
+    
+    function backspace() {
+        var input_var = document.getElementById('calc-output');
+        var x = input_var.value;
+        if (x.length > 0) {
+            x = x.substring(0, x.length - 1);
+            input_var.value = x;
+        }
+    }
+    
+    function compute() {
+        var input_var = document.getElementById('calc-output');
+        ans = (+eval(input_var.value));
+        document.getElementById('calc-output').value = '=' + ans;
+    }
